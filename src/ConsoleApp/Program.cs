691977eae9ef85baf079e72332c35c8f26288147
Sander Molenkamp <sander@sandermolenkamp.com>
2024-09-22 12:49:26 +0200

    Add fake auth

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+
+await CheckInAsync();
+await AccessSecurityFastLaneAsync();
+await AccessLoungeAsync();
+return;
+
+
+static async Task CheckInAsync()
+{
+    var response = await CallApiAsync("/check-in", new Claim(ClaimTypes.Role, "passenger"));
+    if (response.IsSuccessStatusCode)
+    {
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+    }
+    else
+    {
+        Console.WriteLine($"Failed ({response.StatusCode})!");
+    }
+}
+
+static async Task AccessSecurityFastLaneAsync()
+{
+    var response = await CallApiAsync("/access-fast-lane", new Claim("checked_in", "true"), new Claim("frequent_flyer_status", "gold"));
+    if (response.IsSuccessStatusCode)
+    {
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+    }
+    else
+    {
+        Console.WriteLine($"Failed ({response.StatusCode})!");
+    }
+}
+
+static async Task AccessLoungeAsync()
+{
+    var response = await CallApiAsync("/access-lounge", new Claim("checked_in", "true"), new Claim("frequent_flyer_status", "gold"));
+    if (response.IsSuccessStatusCode)
+    {
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+    }
+    else
+    {
+        Console.WriteLine($"Failed ({response.StatusCode})!");
+    }
+}
+
+
+static Task<HttpResponseMessage> CallApiAsync(string requestUri, params Claim[] claims)
+{
+    var client = new HttpClient();
+    client.BaseAddress = new Uri("https://localhost:7074/");
+
+    var token = GenerateJwtToken(claims);
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+    return client.GetAsync(requestUri);
+}
+
+static string GenerateJwtToken(params Claim[] claims)
+{
+
+    var token = new JwtSecurityToken(
+        claims: claims,
+        expires: DateTime.Now.AddMinutes(10)
+    );
+
+    return new JwtSecurityTokenHandler().WriteToken(token);
+}
--- src/OpenFGATalk/Program.cs
+++ src/OpenFGATalk/Program.cs
@@ -1,58 +1,41 @@
-using Microsoft.AspNetCore.Authentication;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Abstractions;
-using Microsoft.Identity.Web.Resource;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+// Add Authentication and Authorization
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = false,
+            ValidateIssuerSigningKey = false,
+            SignatureValidator = (token, _) => new JsonWebToken(token)
+        };
+    });
 builder.Services.AddAuthorization();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapGet("/check-in", () => "You are checked in!")
+    .RequireAuthorization(configure => configure.RequireRole("passenger"));
 
-app.UseHttpsRedirection();
-
-var scopeRequiredByApi = app.Configuration["AzureAd:Scopes"] ?? "";
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+app.MapGet("/access-fast-lane", () => "You are allowed in the security fast lane!")
+    .RequireAuthorization(configure =>  configure.RequireAssertion(context => 
     {
-        httpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+        var isCheckedIn = context.User.HasClaim(claim => claim is { Type: "checked_in", Value: "true" });
+        var isPremiumFrequentFlyer = context.User.HasClaim(claim => claim is { Type: "frequent_flyer_status", Value: "silver" or "gold" or "platinum" });
+            
+        return isCheckedIn && isPremiumFrequentFlyer;
+    }));
 
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi()
-    .RequireAuthorization();
+app.MapGet("/access-lounge", () => Results.Forbid());
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
